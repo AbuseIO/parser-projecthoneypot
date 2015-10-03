12f671c4df001b1f31b5ff6bbb0b25a1acaced85
Bart Vrancken <bart@abuse.io>
2015-10-03 16:17:11 +0200

    update new layout and update version

--- src/Projecthoneypot.php
+++ src/Projecthoneypot.php
@@ -2,21 +2,14 @@
 
 namespace AbuseIO\Parsers;
 
-use ReflectionClass;
-use Log;
-
 class Projecthoneypot extends Parser
 {
-    public $parsedMail;
-    public $arfMail;
-
     /**
      * Create a new Projecthoneypot instance
      */
     public function __construct($parsedMail, $arfMail)
     {
-        $this->parsedMail = $parsedMail;
-        $this->arfMail = $arfMail;
+        parent::__construct($parsedMail, $arfMail, $this);
     }
 
     /**
@@ -26,70 +19,53 @@ class Projecthoneypot extends Parser
      */
     public function parse()
     {
-        // Generalize the local config based on the parser class name.
-        $reflect = new ReflectionClass($this);
-        $this->configBase = 'parsers.' . $reflect->getShortName();
-
-        Log::info(
-            get_class($this). ': Received message from: '.
-            $this->parsedMail->getHeader('from') . " with subject: '" .
-            $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$this->configBase}.parser.name")
-        );
-
-        // Define array where all events are going to be saved in.
-        $events = [ ];
-
-        // Didn't find an ARF report, go scrape the email body!
-        $body = $this->parsedMail->getMessageBody();
-        preg_match_all(
+        if (preg_match_all(
             '/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) \(([HSDCR])\)\r?\n  \- ([\w, :]+)\r?\n/',
-            $body,
+            $this->parsedMail->getMessageBody(),
             $match
-        );
-        array_shift($match);
-        $reports = [ ];
-        while (sizeof($match[0]) > 0) {
-            $reports[] = [
-                'Source' => array_shift($match[0]),
-                'Type' => array_shift($match[1]),
-                'Date' => array_shift($match[2])
-            ];
+        )) {
+            array_shift($match);
+
+            $reports = [ ];
+            while (sizeof($match[0]) > 0) {
+                $reports[] = [
+                    'Source' => array_shift($match[0]),
+                    'Type' => array_shift($match[1]),
+                    'Date' => array_shift($match[2])
+                ];
+            }
+
+            foreach ($reports as $report) {
+                if (!empty($report['Report-Type'])) {
+                    $this->feedName = $report['Report-Type'];
+
+                    // If feed is known and enabled, validate data and save report
+                    if ($this->isKnownFeed() && $this->isEnabledFeed()) {
+                        // Sanity check
+                        if ($this->hasRequiredFields($report) === true) {
+                            // Event has all requirements met, filter and add!
+                            $report = $this->applyFilters($report);
+
+                            $this->events[] = [
+                                'source'        => config("{$this->configBase}.parser.name"),
+                                'ip'            => $report['Source'],
+                                'domain'        => false,
+                                'uri'           => false,
+                                'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
+                                'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
+                                'timestamp'     => strtotime($report['Date']),
+                                'information'   => json_encode($report),
+                            ];
+                        }
+                    }
+                } else {
+                    $this->warningCount++;
+                }
+            }
+        } else {
+            $this->warningCount++;
         }
 
-        foreach ($reports as $report) {
-            $this->feedName = $report['Type'];
-
-            if (!$this->isKnownFeed()) {
-                return $this->failed(
-                    "Detected feed {$this->feedName} is unknown."
-                );
-            }
-
-            if (!$this->isEnabledFeed()) {
-                continue;
-            }
-
-            if (!$this->hasRequiredFields($report)) {
-                return $this->failed(
-                    "Required field {$this->requiredField} is missing or the config is incorrect."
-                );
-            }
-
-            $report = $this->applyFilters($report);
-
-            $events[] = [
-                'source'        => config("{$this->configBase}.parser.name"),
-                'ip'            => $report['Source'],
-                'domain'        => false,
-                'uri'           => false,
-                'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
-                'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
-                'timestamp'     => strtotime($report['Date']),
-                'information'   => json_encode($report),
-            ];
-        }
-
-        return $this->success($events);
+        return $this->success();
     }
 }
